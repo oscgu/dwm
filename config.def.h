78afb232d2d910298eb6aff177a5da35d735e651
Oscar Guertler <oscarguertler@skiff.com>
2024-03-25 21:41:42 +0100

    chore: pull in upstream

--- config.def.h
+++ config.def.h
@@ -82,7 +82,7 @@ static const Layout layouts[] = {
 /* commands */
 static char dmenumon[2] =
     "0"; /* component of dmenucmd, manipulated in spawn() */
-static const char *dmenucmd[] = {"dmenu_run",    "-fn", dmenufont,   "-nb",
+static const char *dmenucmd[] = {"dmenu_run", "-m", dmenumon,    "-fn", dmenufont,   "-nb",
                                  normbgcolor,    "-nf", normfgcolor, "-sb",
                                  selbordercolor, "-sf", selfgcolor,  NULL};
 static const char *termcmd[] = {"wezterm", NULL};
